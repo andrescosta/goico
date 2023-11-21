23cdeba74ea03265d99ca599da81b4f079353ce3
Andres <andres.costa@gmail.com>
2023-11-21 09:23:34 -0500

    ver. 1

---
+++ pkg/service/service.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"context"
+	"os/signal"
+	"syscall"
+
+	"github.com/andrescosta/goico/pkg/config"
+	"github.com/andrescosta/goico/pkg/log"
+)
+
+type Service func(context.Context) error
+
+func Start(service Service) {
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+
+	err := config.LoadEnvVariables()
+
+	logger := log.NewUsingEnv()
+
+	ctx = logger.WithContext(ctx)
+
+	if err != nil {
+		logger.Fatal().Msg("Error loading .env file")
+	}
+
+	defer func() {
+		done()
+		if r := recover(); r != nil {
+			logger.Fatal()
+		}
+	}()
+
+	err = service(ctx)
+	done()
+
+	if err != nil {
+		logger.Fatal().Err(err)
+	}
+}
