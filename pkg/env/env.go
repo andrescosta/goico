44b37326ac4f62b316308821dbe9aaefc35b57bc
Andres <andres.costa@gmail.com>
2023-11-25 14:42:26 -0500

    refactors

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -5,34 +5,42 @@ import (
 	"strconv"
 )
 
-func GetAsString(key string, value string) string {
+func GetAsString(key string, value ...string) string {
 	s, ok := os.LookupEnv(key)
 	if !ok {
-		return value
+		return getDefault("", value)
 	}
 	return s
 }
 
-func GetAsInt[T ~int | ~int32 | ~int8 | ~int64](key string, value T) T {
+func GetAsInt[T ~int | ~int32 | ~int8 | ~int64](key string, value ...T) T {
 	s, ok := os.LookupEnv(key)
 	if !ok {
-		return value
+		return getDefault(0, value)
 	}
 	v, err := strconv.Atoi(s)
 	if err != nil {
-		return value
+		return getDefault(0, value)
 	}
 	return T(v)
 }
 
-func GetAsBool(key string, value bool) bool {
+func GetAsBool(key string, value ...bool) bool {
 	s, ok := os.LookupEnv(key)
 	if !ok {
-		return value
+		return getDefault(false, value)
 	}
 	v, err := strconv.ParseBool(s)
 	if err != nil {
-		return value
+		return getDefault(false, value)
 	}
 	return v
+}
+
+func getDefault[T any](default1 T, values []T) T {
+	if len(values) == 0 {
+		return default1
+	} else {
+		return values[0]
+	}
 }
