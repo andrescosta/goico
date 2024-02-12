ed7e055d577524f739178f3ff76f092e049b395d
Andres <andres.costa@gmail.com>
2024-02-11 20:26:44 -0500

    added pprof support.

---
+++ pkg/service/profiling.go
@@ -0,0 +1,14 @@
+package service
+
+import (
+	"context"
+	"net/http"
+	_ "net/http/pprof" //nolint:gosec
+
+	"github.com/gorilla/mux"
+)
+
+func ConfigProfilingHandlers(_ context.Context, router *mux.Router) error {
+	router.PathPrefix("/debug/pprof/").Handler(http.DefaultServeMux)
+	return nil
+}
