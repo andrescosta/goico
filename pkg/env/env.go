843aad5492d9c6c972e496b1db3c30ef400baff1
Andres <andres.costa@gmail.com>
2024-02-24 15:01:23 -0500

    new test cases. moves env getters to its own file.

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -5,9 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"slices"
-	"strconv"
 	"strings"
-	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -24,76 +22,13 @@ const (
 
 var environments = []string{Development, Production, Test}
 
-func String(key string, defs ...string) string {
-	s, ok := os.LookupEnv(key)
-	if !ok {
-		return getDefault(defs, "")
-	}
-	return s
-}
-
-func StringOrNil(key string) *string {
-	s, ok := os.LookupEnv(key)
-	if !ok {
-		return nil
-	}
-	return &s
-}
-
-func Duration(key string, values ...time.Duration) *time.Duration {
-	def := func(v []time.Duration) *time.Duration {
-		if len(v) == 0 {
-			return nil
-		}
-		return &v[0]
-	}
-	s := StringOrNil(key)
-	if s == nil {
-		return def(values)
-	}
-	r, err := time.ParseDuration(*s)
-	if err != nil {
-		return def(values)
-	}
-	return &r
-}
-
-func Int[T ~int | ~int32 | ~int8 | ~int64](key string, value ...T) T {
-	s, ok := os.LookupEnv(key)
-	if !ok {
-		return getDefault(value, 0)
-	}
-	v, err := strconv.Atoi(s)
-	if err != nil {
-		return getDefault(value, 0)
-	}
-	return T(v)
-}
-
-func Bool(key string, value ...bool) bool {
-	s, ok := os.LookupEnv(key)
-	if !ok {
-		return getDefault(value, false)
-	}
-	v, err := strconv.ParseBool(s)
-	if err != nil {
-		return getDefault(value, false)
-	}
-	return v
-}
-
-func Array(key string, def string) []string {
-	v := String(key, def)
-	return strings.Split(v, ",")
-}
-
 // Follows this convention:
 //
 //	https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
 func Load(name string) (bool, string, error) {
 	loaded := false
 
-	// "basedir" set
+	// --env:[variable]=[value] set
 	if err := setEnvsUsingCommandLineArgs(); err != nil {
 		return false, "", err
 	}
@@ -105,45 +40,34 @@ func Load(name string) (bool, string, error) {
 			return false, "", fmt.Errorf("invalid environment %s", environment)
 		}
 	}
+	// .env.[environment].local
 	if err := load(true, ".env."+environment+".local"); err == nil {
 		loaded = true
 	}
 
 	if environment != "test" {
+		// .env.local
 		if err := load(true, ".env.local"); err == nil {
 			loaded = true
 		}
 	}
 
+	//.env.local.[environment]
 	if err := load(false, ".env."+environment); err == nil {
 		loaded = true
 	}
 
+	//.env.[environment]
 	if err := load(false, ".env."+name); err == nil {
 		loaded = true
 	}
 
+	// .env
 	if err := load(false, fileDefault); err == nil {
 		loaded = true
 	}
 
 	return loaded, environment, nil
-}
-
-func Workdir() string {
-	defaultDir := fmt.Sprint(".", string(os.PathSeparator))
-	return String(WorkDirVar, defaultDir)
-}
-
-func Basedir() string {
-	defaultDir := fmt.Sprint(".", string(os.PathSeparator))
-	return String(BaseDirVar, defaultDir)
-}
-
-func WorkdirPlus(elem ...string) (ret string) {
-	elem = append([]string{Workdir()}, elem...)
-	ret = filepath.Join(elem...)
-	return
 }
 
 func SetargsV(name string, value string) {
@@ -168,11 +92,4 @@ func load(override bool, files ...string) (err error) {
 		}
 	}
 	return
-}
-
-func getDefault[T any](values []T, default1 T) T {
-	if len(values) == 0 {
-		return default1
-	}
-	return values[0]
 }
